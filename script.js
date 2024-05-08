b2987f676cd8d2cb5f9528588b6f1ee87374338c
para-commando <anirudh.nayak.172k@gmail.com>
2024-05-08 18:14:22 +0530

    playlist section design 4

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log('olaaaaaaa0');  
+async function getAllSongs() {
+  let reponse = await fetch('http://127.0.0.1:3080/music/');
+  reponse = await reponse.text();
+  let div = document.createElement('div');
+  div.innerHTML = reponse;
+  let aTags = div.getElementsByTagName('a');
+  let songsList = [];
+  for (let index = 0; index < aTags?.length; index++) {
+    const element = aTags[index];
+    if (element.href.endsWith('.mp3')) {
+      songsList.push(element.href);
+    }
+  }
+  console.log("🚀 ~ getAllSongs ~ songsList:", songsList)
+  return;
+
+}
+console.log(getAllSongs());
+
